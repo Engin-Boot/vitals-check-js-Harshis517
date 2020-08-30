326e5777dc2831d147421374c899b491b8b6b942
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-08-31 01:02:36 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,17 +1,24 @@
-const expect = require('chai').expect;
-//const b=require("./bpmCheck.js");
-const respChecker=require("./resp_heck.js");
-const spo2Checker=require("./spo_check.js");
-const bpmChecker=require("./bpm_check.js")
-function vitalsAreOk(bpm, spo2, respRate) {
-    if(  (bpmChecker(bpm)==false) || (respChecker(respRate)==false) || (spo2Checker(spo2)==false) ) {
+
+const alert = require('./vital_breach_alerter');
+const vitaltypes = require('./vitaltypes');
+
+function isvitalok(vitaltype, vital_value) {
+
+    let min = vitaltypes[vitaltype][0];
+    let max = vitaltypes[vitaltype][1]; 
+
+    if(vital_value < min ){
+        //console.log(vital_value);
+        alert({[vitaltype]:"low"});
         return false;
     }
-    console.log("Vitals are under control");
+    if(vital_value > max){
+        console.log(vital_value);
+        alert({[vitaltype]: "high"});
+        return false;
+    }
+    
     return true;
 }
 
-expect(vitalsAreOk(100, 95, 70)).to.be.true
-expect(vitalsAreOk(50, 95, 70)).to.be.false;
-//vitalsAreOk(100,95,70);
-console.log('checker is done');
+module.exports = isvitalok;
